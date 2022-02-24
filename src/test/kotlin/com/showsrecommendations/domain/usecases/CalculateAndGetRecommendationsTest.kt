cd0a75af4ee362414dd839ff4b94adf3d4c65527
Federico Mete <>
2022-02-24 12:52:11 -0300

    Get & CalculateAndGet Recommendations tests updated with Mockk

--- src/test/kotlin/com/showsrecommendations/domain/usecases/CalculateAndGetRecommendationsTest.kt
+++ src/test/kotlin/com/showsrecommendations/domain/usecases/CalculateAndGetRecommendationsTest.kt
@@ -5,28 +5,35 @@ import com.showsrecommendations.adapters.repositories.inmemory.UsersRepositoryIn
 import com.showsrecommendations.domain.entities.FollowedUser
 import com.showsrecommendations.domain.entities.Recommendation
 import com.showsrecommendations.domain.entities.Review
+import com.showsrecommendations.domain.ports.ReviewsRepository
+import com.showsrecommendations.domain.ports.UsersRepository
+import io.mockk.every
+import io.mockk.mockk
 
 import org.junit.jupiter.api.Assertions.*
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 
-class CalculateAndGetRecommendationsTest: Spek({
+class CalculateAndGetRecommendationsTest : Spek({
 
     Feature("2.1 CalculateAndGetRecommendations") {
 
-        val reviewsRepository = ReviewsRepositoryInMemory()
-        val usersRepository = UsersRepositoryInMemory()
+        val reviewsRepository = mockk<ReviewsRepository>(relaxed = true)
+        val usersRepository = mockk<UsersRepository>(relaxed = true)
 
         val calculateAndGetRecommendations = CalculateAndGetRecommendations(
-            usersRepository= usersRepository,
-            reviewsRepository = reviewsRepository)
+            usersRepository = usersRepository,
+            reviewsRepository = reviewsRepository
+        )
 
         var recommendedShows: List<Recommendation> = listOf()
 
         Scenario("2.1.1 Calculating and getting recommendations") {
 
-            Given("user1 that is following user2, user3, user4, user5 and user6 ")  {
-                usersRepository.followedUsers["user1"] = listOf(
+            Given("user1 that is following user2, user3, user4, user5 and user6 ") {
+                every {
+                    usersRepository.getFollowedUsers(userId = "user1")
+                } returns listOf(
                     FollowedUser(id = "user2", followedDate = "20220218"),
                     FollowedUser(id = "user3", followedDate = "20220218"),
                     FollowedUser(id = "user4", followedDate = "20220218"),
@@ -35,47 +42,49 @@ class CalculateAndGetRecommendationsTest: Spek({
                 )
             }
 
-            And("user1 has seen and recommended Movie VI"){
-                reviewsRepository["user1"] = mutableListOf(
-                    Review(showId = "MovieVI", rating =1f, createdDate = "20220218")
+            And("user1 has seen and recommended Movie VI") {
+                every {
+                    reviewsRepository.getReviews(userId = "user1")
+                } returns listOf(
+                    Review(showId = "MovieVI", rating = 1f, createdDate = "20220218")
                 )
             }
 
-            And("user2 has recommended Movie I, Movie III, Movie V and Movie VI"){
-                reviewsRepository["user2"] = mutableListOf(
-                    Review(showId = "MovieI", rating =1f, createdDate = "20220218"),
-                    Review(showId = "MovieIII", rating =1f, createdDate = "20220218"),
-                    Review(showId = "MovieV", rating =1f, createdDate = "20220218"),
-                    Review(showId = "MovieVI", rating =1f, createdDate = "20220218")
+            And("user2 has recommended Movie I, Movie III, Movie V and Movie VI") {
+                every {
+                    reviewsRepository.getReviews(userId = "user2")
+                } returns listOf(
+                    Review(showId = "MovieI", rating = 1f, createdDate = "20220218"),
+                    Review(showId = "MovieIII", rating = 1f, createdDate = "20220218"),
+                    Review(showId = "MovieV", rating = 1f, createdDate = "20220218"),
+                    Review(showId = "MovieVI", rating = 1f, createdDate = "20220218")
                 )
             }
 
-            And("user3 has recommended Movie I, and unrecommended Movie III & Movie V"){
-                reviewsRepository["user3"] = mutableListOf(
-                    Review(showId = "MovieI", rating =1f, createdDate = "20220218"),
-                    Review(showId = "MovieIII", rating =0f, createdDate = "20220218"),
-                    Review(showId = "MovieV", rating =0f, createdDate = "20220218"),
+            And("user3 has recommended Movie I, and unrecommended Movie III & Movie V") {
+                every {
+                    reviewsRepository.getReviews(userId = "user3")
+                } returns listOf(
+                    Review(showId = "MovieI", rating = 1f, createdDate = "20220218"),
+                    Review(showId = "MovieIII", rating = 0f, createdDate = "20220218"),
+                    Review(showId = "MovieV", rating = 0f, createdDate = "20220218")
                 )
             }
 
-            And("user4, user5 and user7 have recommended Movie I and Movie III"){
-                reviewsRepository["user4"] = mutableListOf(
-                    Review(showId = "MovieI", rating =1f, createdDate = "20220218"),
-                    Review(showId = "MovieIII", rating =1f, createdDate = "20220218")
-                )
-                reviewsRepository["user5"] = mutableListOf(
-                    Review(showId = "MovieI", rating =1f, createdDate = "20220218"),
-                    Review(showId = "MovieIII", rating =1f, createdDate = "20220218")
-                )
-                reviewsRepository["user7"] = mutableListOf(
-                    Review(showId = "MovieI", rating =1f, createdDate = "20220218"),
-                    Review(showId = "MovieIII", rating =1f, createdDate = "20220218")
+            And("user4, user5 and user7 have recommended Movie I and Movie III") {
+                every {
+                    reviewsRepository.getReviews(match { it in listOf("user4", "user5", "user7") })
+                } returns listOf(
+                    Review(showId = "MovieI", rating = 1f, createdDate = "20220218"),
+                    Review(showId = "MovieIII", rating = 1f, createdDate = "20220218")
                 )
             }
 
-            And("user6 has recommended Movie III"){
-                reviewsRepository["user6"] = mutableListOf(
-                    Review(showId = "MovieIII", rating =1f, createdDate = "20220218")
+            And("user6 has recommended Movie III") {
+                every {
+                    reviewsRepository.getReviews(userId = "user6")
+                } returns listOf(
+                    Review(showId = "MovieIII", rating = 1f, createdDate = "20220218")
                 )
             }
 
@@ -83,13 +92,15 @@ class CalculateAndGetRecommendationsTest: Spek({
                 recommendedShows = calculateAndGetRecommendations(userId = "user1")
             }
 
-            Then("user1 sees Movie I with 4 recommendations, Movie III with 4 recommendations and 1 un-recommendation and Movie V with 1 recommendation and 1 un-recommendation"){
+            Then("user1 sees Movie I with 4 recommendations, Movie III with 4 recommendations and 1 un-recommendation and Movie V with 1 recommendation and 1 un-recommendation") {
                 assertEquals(3, recommendedShows.size)
-                assertEquals(recommendedShows, listOf(
-                    Recommendation(showId = "MovieI", positiveReviewsQty = 4, negativeReviewsQty = 0),
-                    Recommendation(showId = "MovieIII", positiveReviewsQty = 4, negativeReviewsQty = 1),
-                    Recommendation(showId = "MovieV", positiveReviewsQty = 1, negativeReviewsQty = 1)
-                ))
+                assertEquals(
+                    recommendedShows, listOf(
+                        Recommendation(showId = "MovieI", positiveReviewsQty = 4, negativeReviewsQty = 0),
+                        Recommendation(showId = "MovieIII", positiveReviewsQty = 4, negativeReviewsQty = 1),
+                        Recommendation(showId = "MovieV", positiveReviewsQty = 1, negativeReviewsQty = 1)
+                    )
+                )
             }
         }
     }
--- src/test/kotlin/com/showsrecommendations/domain/usecases/GetRecommendationsTest.kt
+++ src/test/kotlin/com/showsrecommendations/domain/usecases/GetRecommendationsTest.kt
@@ -4,48 +4,59 @@ import com.showsrecommendations.adapters.repositories.inmemory.RecommendationsRe
 import com.showsrecommendations.adapters.repositories.inmemory.ReviewsRepositoryInMemory
 import com.showsrecommendations.domain.entities.Recommendation
 import com.showsrecommendations.domain.entities.Review
+import com.showsrecommendations.domain.ports.RecommendationsRepository
+import com.showsrecommendations.domain.ports.ReviewsRepository
+import com.showsrecommendations.domain.ports.UsersRepository
+import io.mockk.every
+import io.mockk.mockk
 
 import org.junit.jupiter.api.Assertions.*
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 
-class GetRecommendationsTest: Spek({
+class GetRecommendationsTest : Spek({
 
     Feature("2.1 GetRecommendations") {
 
-        val reviewsRepository = ReviewsRepositoryInMemory()
-        val recommendationsRepository = RecommendationsRepositoryInMemory()
+        val reviewsRepository = mockk<ReviewsRepository>(relaxed = true)
+        val recommendationsRepository = mockk<RecommendationsRepository>(relaxed = true)
 
         val getRecommendations = GetRecommendations(
-            recommendationsRepository= recommendationsRepository,
-            reviewsRepository = reviewsRepository)
+            recommendationsRepository = recommendationsRepository,
+            reviewsRepository = reviewsRepository
+        )
 
         var recommendedShowsIds: List<String> = listOf()
 
         Scenario("2.1.2 Getting recommendations") {
 
-            Given("some recommendations already calculated for user1")  {
+            Given("some recommendations already calculated for user1") {
 
-                recommendationsRepository["user1"] = listOf(
+                every {
+                    recommendationsRepository.getRecommendations(userId = "user1")
+                } returns listOf(
                     Recommendation(showId = "MovieI", positiveReviewsQty = 4, negativeReviewsQty = 0),
                     Recommendation(showId = "MovieIII", positiveReviewsQty = 4, negativeReviewsQty = 1),
                     Recommendation(showId = "MovieV", positiveReviewsQty = 1, negativeReviewsQty = 1),
                     Recommendation(showId = "MovieVI", positiveReviewsQty = 1, negativeReviewsQty = 0)
                 )
-
             }
 
-            And("one of them was already reviewed by user1"){
-                reviewsRepository["user1"] = mutableListOf(Review(showId = "MovieVI", rating = 1f, createdDate = "20220218"))
+            And("one of them was already reviewed by user1") {
+                every {
+                    reviewsRepository.getReviews(userId = "user1")
+                } returns listOf(
+                    Review(showId = "MovieVI", rating = 1f, createdDate = "20220218")
+                )
             }
 
             When("user1 gets its recommendations") {
-                recommendedShowsIds = getRecommendations(userId = "user1").map {it.showId}
+                recommendedShowsIds = getRecommendations(userId = "user1").map { it.showId }
             }
 
-            Then("the reviewed show is removed and the rest are ordered by its rating (desc)"){
+            Then("the reviewed show is removed and the rest are ordered by its rating (desc)") {
                 assertEquals(3, recommendedShowsIds.size)
-                assertEquals(recommendedShowsIds, listOf("MovieI","MovieIII", "MovieV"))
+                assertEquals(recommendedShowsIds, listOf("MovieI", "MovieIII", "MovieV"))
             }
         }
     }
